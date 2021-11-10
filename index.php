c453b860e6b25ee84fb7a46e96c659baf9203229
Karina Pineda <90991228+karinaAF119@users.noreply.github.com>
2021-11-10 10:33:15 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,323 @@
+
+ 
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Pagina Bomberos</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+    <!-- Icon -->
+    <link rel="stylesheet" href="assets/fonts/line-icons.css">
+    <!-- Owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.css">
+    
+    <!-- Animate -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+  </head>
+  <body>
+
+    <!-- bara de navegacion -->
+    <header id="header-wrap">
+      <!-- Navbar Start -->
+      <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <a href="index.html" class="navbar-brand"><img src="assets/img/bombero.png" alt="logo"></a>       
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="lni-menu"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto w-100 justify-content-end clearfix">
+              <li class="nav-item active">
+                <a class="nav-link" href="#hero-area">
+                   Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#services">
+                 Funcionalidades
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#historia">
+                   Acerca de
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#team">
+                  Testimonios
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./Login.php">
+             Admin
+                </a>
+              </li>
+
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- Final de barra -->
+
+      <!-- Primera vista -->
+      <div id="hero-area" class="hero-area-bg">
+        <div class="container">      
+          <div class="row">
+            <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+              <div class="contents">
+                <h2>Estación No.46<br> Palencia</h2>
+                <p>Asociación Nacional de Bomberos Municipales Departamentales ASONBOMD Estación No.46 Palencia</p>
+              </div>
+            </div>
+            <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+              <div class="intro-img">
+                <img class="img-fluid" src="assets/img/bom2.png" alt="">
+              </div>            
+            </div>
+          </div> 
+        </div> 
+      </div>
+      <!-- Final de la primera vista-->
+
+    </header>
+       <!-- Final de la primera vista-->
+
+    <!-- Funcionalidades -->
+    <section id="services" class="section-padding">
+      <div class="container">
+        <div class="section-header text-center">
+          <h2 class="section-title wow fadeInDown" data-wow-delay="0.3s">Funcionalidades</h2>
+          <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div>
+        </div>
+        <div class="row">
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-4 col-xs-12">
+            <div class="services-item wow fadeInRight" data-wow-delay="0.3s">
+              <div class="icon">
+                <i class="lni-cog"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#">Facil Interacción</a></h3>
+                <p>  </p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-4 col-xs-12">
+            <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+              <div class="icon">
+                <i class="lni-stats-up"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#">Aumento en el Rendimiento
+                </a></h3>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-4 col-xs-12">
+            <div class="services-item wow fadeInRight" data-wow-delay="0.9s">
+              <div class="icon">
+                <i class="lni-users"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#">Seguridad</a></h3>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-4 col-xs-12">
+            <div class="services-item wow fadeInRight" data-wow-delay="1.2s">
+              <div class="icon">
+                <i class="lni-layers"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#">Optimizacion</a></h3>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-4 col-xs-12">
+            <div class="services-item wow fadeInRight" data-wow-delay="1.5s">
+              <div class="icon">
+                <i class="lni-mobile"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#">Adaptabilidad</a></h3>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+          <!-- Services item -->
+          <div class="col-md-6 col-lg-4 col-xs-12">
+            <div class="services-item wow fadeInRight" data-wow-delay="1.8s">
+              <div class="icon">
+                <i class="lni-rocket"></i>
+              </div>
+              <div class="services-content">
+                <h3><a href="#">Interfaces Amigables</a></h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Services Section End -->
+
+
+
+
+    <section id="historia" class="section-padding">
+    <!-- About Section start -->
+    <div class="about-area section-padding bg-gray">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 col-xs-12 info">
+            <div class="about-wrapper wow fadeInLeft" data-wow-delay="0.3s">
+              <div>
+                <div class="site-heading">
+                
+                  <h2 class="section-title">Historia Estación No.46 Palencia </h2>
+                </div>
+                <div class="content">
+                  <p>
+                    La labor de un bombero para muchas personas simplemente es apagar el fuego en una
+                     emergencia, sin embargo, la realidad con la que día a día se enfrentan es muy diferente, pues su labor primordial es salvar vidas y buscar en todo momento el bienestar del municipio,
+                      la Estación de Bomberos Palencia fue fundada en el año 2006, siendo Frank Bococ el donador del
+                       terreno para la estación, la donación de materiales para su construcción fue donado por migrantes
+                        Palencianos que radican en Los Estados Unidos, la primera ambulación fue donación del Licenciado Fredy
+                         Nazem Yunes con el apoyo del diputado Antonio Coro. (Estacion Palencia #46 Historia , s.f.) </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12 wow fadeInRight" data-wow-delay="0.3s">
+            <img class="img-fluid" src="./assets/img/portada bom.jpg" alt="" >
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About Section End -->
+  </section>
+
+    
+
+    <!-- Team Section Start -->
+    <section id="team" class="section-padding bg-gray">
+      <div class="container">
+        <div class="section-header text-center">          
+          <h2 class="section-title wow fadeInDown" data-wow-delay="0.3s">Comentarios</h2>
+          <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.2s">
+              <div class="team-img">
+                <img class="img-fluid" src="assets/img/beto1.png" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">Guadalupe Alberto Reyes</a></h3>
+                  <p>Alcalde Municipal</p>
+                </div>
+                <p>Considero importante que se valore y se agradezca el labor que realizan los bomberos al ofrecer un servicio de calidad a cada palenciano.</p>
+                <ul class="social-icons">
+               
+                </ul>
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.4s">
+              <div class="team-img">
+                <img class="img-fluid" src="assets/img/profe.jpg" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">Vilmer Mijangos</a></h3>
+                  <p>Licenciado</p>
+                </div>
+                <p>Es importante dar a conocer los servicios que presta la Estación No.46 y la importancia que tiene de poder colaborar con ellos</p>
+                <ul class="social-icons">
+                </ul>
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.6s">
+              <div class="team-img">
+                <img class="img-fluid" src="assets/img/Bom2yu.jpg" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">José Abrego</a></h3>
+                  <p>Bombero Estación No.46</p>
+                </div>
+                <p>Como miembro más joven de la unidad considero importante el labor de cada compañero bombero al prestar un servicio en beneficio de nuestro Municipio</p>
+               
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+            <!-- Team Item Ends -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Team Section End -->
+
+  
+   
+
+ 
+
+    
+
+    <!-- Go to Top Link -->
+    <a href="#" class="back-to-top">
+    	<i class="lni-arrow-up"></i>
+    </a>
+    
+    <!-- Preloader -->
+    <div id="preloader">
+      <div class="loader" id="loader-1"></div>
+    </div>
+    <!-- End Preloader -->
+    
+    <!-- Funcinalidades de JavaScrip para las animaciones -->
+    <script src="assets/js/jquery-min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/jquery.nav.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>  
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.min.js"></script>
+      
+  </body>
+</html>
